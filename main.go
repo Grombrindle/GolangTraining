2e7ec7f1711a363a08b44579d52515358e9064ca
Grombrindle <everythingiswronj@gmail.com>
2025-05-30 19:18:00 +0300

    used a post request to make an object and printed in the teminal, what ive learned: 1-how to use post to send to a url and make somthing out of it 2-how to make a body into json suing marshal 3-how to print somethings in the localhost 4-how to use the vars that come with the url 5-adding pages to the site and maybe adding files to them 6-goroutine but still to early for it

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	web "helloworld/Websocket"
+	gg "helloworld/modules"
+	"log"
+	// "net/http"
+	// "rsc.io/quote"
+)
+
+func Go() string {
+	return "kaka wasaka"
+}
+
+func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+
+	web.HttpCall()
+
+	for i := 0; i < 10; i++ {
+		println("")
+	}
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	var seq []uint64
+	fmt.Println(gg.FibSeqRecursive(7, seq))
+
+	// Request a greeting message.
+	message, err := gg.Hello("dasdsad")
+
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
+	fmt.Println(message)
+}
---
+++ webSocket/http.go
@@ -0,0 +1,87 @@
+package Websocket
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	http "net/http"
+	"time"
+
+	"bytes"
+
+	"github.com/gorilla/mux"
+	// route "gorilla/mux"
+)
+
+func Post() {
+	time.Sleep(1 * time.Second)
+	title := "some_title"
+	url := fmt.Sprintf("http://localhost:8000/books/%s", title)
+	bodyData := map[string]string{"title": title}
+	bodyBytes, err := json.Marshal(bodyData)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp, err := http.Post(url, "application/json", bytes.NewReader(bodyBytes))
+	if err != nil {
+		log.Fatal(err)
+	}
+	// vars := mux.Vars()
+	// title := "da"
+	// resp =  title
+	defer resp.Body.Close()
+
+	log.Println("Response status:", resp.Status, title)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+	fmt.Println(string(body))
+}
+func HttpCall() {
+	r := mux.NewRouter()
+	fs := http.FileServer(http.Dir("/static"))
+	// r.HandleFunc("/books/{title}", ReadBook).Methods("GET")
+	// r.HandleFunc("/books/{title}", UpdateBook).Methods("PUT")
+	// r.HandleFunc("/books/{title}", DeleteBook).Methods("DELETE")
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+
+		fmt.Fprintf(w, "helloworld ma nigga: %s\n", r.URL.Path)
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	})
+	r.HandleFunc("/books/{title}", CreateBook).Methods("POST")
+	r.Handle("/static", http.StripPrefix("/static", fs))
+	go http.ListenAndServe(":8000", r)
+	Post()
+	// fmt.Println("dsadsa")
+	// Restrict the request handler to specific hostnames or subdomains.
+
+	// r.HandleFunc("/books/{title}", BookHandler).Host("www.mybookstore.com")
+
+	// Restrict the request handler to http/https.
+
+	// r.HandleFunc("/secure", SecureHandler).Schemes("https")
+	// r.HandleFunc("/insecure", InsecureHandler).Schemes("http")
+
+	// Restrict the request handler to specific path prefixes.
+
+	// 	bookrouter := r.PathPrefix("/books").Subrouter()
+	// bookrouter.HandleFunc("/", AllBooks)
+	// bookrouter.HandleFunc("/{title}", GetBook)
+
+	// fs := http.FileServer(http.Dir("/static"))
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintf(w, "helloworld ma nigga: %s\n", r.URL.Path)
+	// 	// fmt.Fprintf(w, "shit is: %s\n", r.URL.Query().Get("token"))
+	// 	// fmt.Print(r.URL.Query().Get("token"))
+	// })
+}
